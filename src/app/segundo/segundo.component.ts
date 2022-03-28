7576357a6e0bbc276c018d4d3b6fc1d485b9908a
ivan <idimitryz@gmail.com>
2022-03-28 22:13:42 +0200

    add data binding por interpolaçao

--- src/app/segundo/segundo.component.ts
+++ src/app/segundo/segundo.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-segundo',
-  templateUrl: './segundo.component.html',
-  styleUrls: ['./segundo.component.css']
+  template: `
+    <p>Bem vinda, {{ nome }}!</p>
+    <p>2 + 8 = {{ 2 + 8 }}</p>
+    <p>{{ 5 + numero }}</p>
+    <p>{{ 20 > 4 }}</p>
+    <p>{{ geraSaudacao() }}</p>
+    <p>{{ nome.toUpperCase() }}</p>`,
+
+  styleUrls: ['./segundo.component.css'],
 })
-export class SegundoComponent implements OnInit {
+export class SegundoComponent {
+  public nome: string = 'Julia';
+  public numero: number = -1;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public geraSaudacao(): string {
+    const saudacao = `Bem vindo, ${this.nome}`;
+    return saudacao;
   }
-
 }
