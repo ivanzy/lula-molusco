586adff8479afbb1d033344a30c8ff3b48ae107a
ivan <idimitryz@gmail.com>
2022-03-28 21:27:40 +0200

    primeiro commit da app angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PrimeiroComponent } from './primeiro.component';
+import { SegundoComponent } from './segundo/segundo.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrimeiroComponent,
+    SegundoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/primeiro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'etvaldo',
+  template: '<p> oi, eu sou um componente legal :) </p>'
+})
+export class PrimeiroComponent{}
